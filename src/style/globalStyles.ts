9e50acb5d1abe2e08a0fe11191d7eda49d422587
Asher <greatasher7@gmail.com>
2023-08-17 11:05:49 +0900

    add fonts

--- src/style/globalStyles.ts
+++ src/style/globalStyles.ts
@@ -2,11 +2,32 @@
 
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import "./fonts.css";
 
 export const GlobalStyles = createGlobalStyle`
-    ${reset}
-    *, *::after, *::before{
-    box-sizing: border-box;
+  ${reset}
+
+  *, *::before, *::after{
+        box-sizing: border-box;
+        color: inherit;
+    }
+  body{
+    font-family: "FontTest";
   }
+  input, textarea, select, button {
+        outline: none;
+        font-size: inherit;
+        font-family: inherit;
+     }
+    input:focus, textarea:focus, select:focus {
+        outline: none;
+    }
+    a{
+        color: inherit;
+        text-decoration: none;
+    }
+    ol, ul, li {  
+        list-style: none;
+    }
 
 `;
