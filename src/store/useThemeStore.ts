fd01acdb959ea857f179b26bb374750557d63894
Asher <greatasher7@gmail.com>
2023-08-29 10:57:41 +0900

    themeStore state on localStorage

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { produce } from "immer";
-import { E_THEME } from "@/types/constants";
+import { E_THEME, THEME_LOCALSTORAGE } from "@/types/constants";
 
 interface IState {
   theme: E_THEME;
@@ -15,12 +15,15 @@ const useThemeStore = create<IState & { actions: IAction }>()(
   devtools((set) => ({
     theme: E_THEME.light,
     actions: {
-      setTheme: (newTheme: E_THEME) =>
+      setTheme: (newTheme: E_THEME) => {
         set(
           produce((state: IState) => {
             state.theme = newTheme;
           })
-        ),
+        );
+
+        window.localStorage.setItem(THEME_LOCALSTORAGE, JSON.stringify(newTheme));
+      },
     },
   }))
 );
